d617732bca6e9d9f9b43a912f83c165a09753bf8
Justin Shepherd <justin.l.shepherd@gmail.com>
2019-11-14 08:54:03 -0600

    Added Buttons, Image, and Code

--- GitHubChallenge/ViewController.swift
+++ GitHubChallenge/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    var resultImageView = ""
+    
+    @IBAction func firstImage(_ sender: Any) {
+        resultImageView = "DioBrando"
+        imageView.image = UIImage(named: resultImageView)
+    }
+    @IBAction func secondImage(_ sender: Any) {
+        resultImageView = "DeepFriedlmao"
+        imageView.image = UIImage(named: resultImageView)
+    }
+    @IBAction func thirdImage(_ sender: Any) {
+        resultImageView = "Dire"
+        imageView.image = UIImage(named: resultImageView)
+    }
+    @IBAction func fourthImage(_ sender: Any) {
+        resultImageView = "ThunderCrossSplitAttack"
+        imageView.image = UIImage(named: resultImageView)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
 }
 
